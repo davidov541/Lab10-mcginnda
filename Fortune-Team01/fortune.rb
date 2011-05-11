4174635e43b061f5cfcf57d217791995d3b844f7
David McGinnis <mcginnda@rose-hulman.edu>
2011-05-10 21:00:32 -0400

    Given code is not working

--- Fortune-Team01/fortune.rb
+++ Fortune-Team01/fortune.rb
@@ -1,7 +1,11 @@
 require 'sinatra/base'
 require 'haml'
 require 'thin'
+require 'fortunedata'
 
 class Fortune < Sinatra::Base
-
+	get '/fortune/random/?' do
+		@fortune = $fortunes[rand($fortunes.size)]
+		haml :fortune
+	end
 end
